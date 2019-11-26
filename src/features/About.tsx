cfe2f31d6986aef489614a3ac0bff8f70f45bd2d
Andy Allison <andy@talkbe.com>
2019-11-26 06:19:06 +0000

    feat(lists and keys): adding some examp[les from lists and keys

--- src/features/About.tsx
+++ src/features/About.tsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
+function Blog(props: any) {
+    const sidebar = (
+        <ul>
+            {props.posts.map((post: any) =>
+                <li key={post.id}>
+                    {post.title}
+                </li>
+            )}
+        </ul>
+    );
+    const content = props.posts.map((post: any) =>
+        <div key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+        </div>
+    );
+    return (
+        <div>
+            {sidebar}
+            <hr />
+            {content}
+        </div>
+    );
+}
+
+const posts = [
+    {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+    {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
 const About = () => {
-    return <h2>About</h2>;
+    return <Blog posts={posts} />
 };
 
 export default About;
